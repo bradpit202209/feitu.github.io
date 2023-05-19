db67af450d9262507c08e18d300a1a901b496802
feitutv <114878191+feitutv@users.noreply.github.com>
2023-05-19 13:12:44 -0400

    Update publish.js

--- publish.js
+++ publish.js
@@ -1,23 +1,20 @@
 var emails = [
-    'dizhi9191@gmail.com'
+    'contact@feitu.tv'
 ];
 var newestUrls = [
-    'https://jiuse001.com',
-	'https://jiuse002.com',
-    'https://jiuse.icu'
+    'https://www.feitu.tv'
 ];
 var otherUrls = [
-    'https://dizhi91.github.io',
-    'https://jiuse123.com'
+    'https://feitutv.github.io'
 ];
 var foreverUrls = [
-    'https://91porny.com'
+    'https://www.feitu.tv'
 ];
 var notices = [
     '* 我们推荐PC和Andriod手机用户使用Chrome(谷歌)浏览器访问，iPhone用户我们建议您使用手机自带Safria浏览器访问。',
     //'* 为了防止域名劫持而无法访问，请设置手机和PC的DNS，我们推荐使用8.8.8.8和1.1.1.1。',
     '* 大陆地区用户我们建议您可以使用VPN或者代理的方式来访问我们的永久地址。',
-	'* 大陆地区用户我们强烈建议您收藏当前页面(jiuse123.com)。'
+	'* 大陆地区用户我们强烈建议您收藏当前页面(feitutv.github.io)。'
 ];
 
 function createFieldElem(option) {
@@ -66,7 +63,7 @@ window.onload = function () {
     mainElem.appendChild(logoElem);
 
     // newest urls
-    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问九色视频'});
+    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问飞兔影视'});
     mainElem.appendChild(newestFieldElem);
 
     // other urls
